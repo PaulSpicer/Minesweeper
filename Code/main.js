eefd73a06e2fc4d774b1c85d3c7ab7a07dbaf836
Paul <Ozukki@hotmail.co.uk>
2019-06-09 10:09:03 +0100

    Mine adjacency calculated and showing

--- Code/main.js
+++ Code/main.js
@@ -3,15 +3,17 @@ function main()
 	let cellsX = 10, cellsY = 10;
 	let noOfMines = 10;
 
-	let GameState = {
+	let GameState = 
+	{
 		mineLocations: new Set(),
 		cells: new Array(cellsX * cellsY)
 	}
 
 	// TODO: Set Board Size
-	
-	// Generate grid
+	document.documentElement.style.setProperty("--game-rows", cellsX);
+	document.documentElement.style.setProperty("--game-columns", cellsY);
 
+	// Generate grid
 	let gameBoard = document.querySelector('.game-board');
 	for(let i = 0; i < GameState.cells.length; i++)
 	{
@@ -27,7 +29,7 @@ function main()
 		let loop = true;		
 		while (loop)
 		{
-			let rand = Math.floor(Math.random() * 100);
+			let rand = Math.floor(Math.random() * GameState.cells.length);
 			if (!GameState.mineLocations.has(rand))
 			{
 				GameState.mineLocations.add(rand);
@@ -36,19 +38,67 @@ function main()
 		}
 	}
 
+	// Calculate adjacency hints
+	for(let i = 0; i < GameState.cells.length; i++)
+	{
+		let adj = new Map();
+		adj.set("W", i - 1);
+		adj.set("E", i + 1);
+		adj.set("N", i - cellsX);
+		adj.set("S", i + cellsX);
+		adj.set("NW", i - cellsX - 1);
+		adj.set("NE", i - cellsX + 1);
+		adj.set("SW", i + cellsX - 1);
+		adj.set("SE", i + cellsX + 1);
+
+		let count = 0;
+		//Top Edge
+		if (i < cellsX) { 
+			adj.delete("NW");
+			adj.delete("NE");
+			adj.delete("N");
+		}
+		// Left Edge
+		if (i % cellsX == 0) { 
+			adj.delete("W");
+			adj.delete("SW");
+			adj.delete("NW");
+		}
+		// Right Edge
+		if (i % cellsX == cellsX) {
+			adj.delete("E");
+			adj.delete("NE");
+			adj.delete("SE");
+		}
+		// Bottom Edge
+		if (i > (i.length - cellsX)) {
+			adj.delete("S");
+			adj.delete("SW");
+			adj.delete("SE");
+		}
+
+		for (let a of adj.values()) 
+		{
+			for (let b of GameState.mineLocations.values())
+			{
+				if (a == b) {count++;}
+			}
+		}
+		GameState.cells[i].dataset.adjMines = count;
+	}
+
 	for (let cell of GameState.cells)
 	{
 		cell.addEventListener("click", cellClick.bind(GameState));
 	}
 }
 
-
-
 function cellClick(event) {
 	console.log(this);
 	console.log(event);
 
-	switch (event.target.className) {
+	switch (event.target.className) 
+	{
 		case "cell": 
 		{
 			if (this.mineLocations.has(parseInt(event.target.dataset.cellid)))
@@ -67,6 +117,10 @@ function cellClick(event) {
 			{
 				//UNCOVER SQUARE
 				event.target.className = "cell-clicked";
+				if (event.target.dataset.adjMines != 0)
+				{
+					event.target.textContent = event.target.dataset.adjMines;
+				}
 			}
 			break;
 		}
