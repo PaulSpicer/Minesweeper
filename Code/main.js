179c9a74b5551621c3ad0284197e5e6fc4955b39
Paul <Ozukki@hotmail.co.uk>
2019-06-09 16:27:17 +0100

    Flood fill logic implemented Bug fixed in adjacency calculations

--- Code/main.js
+++ Code/main.js
@@ -1,10 +1,9 @@
-function main()
-{
+function main() {
 	// TODO: User Set Board Size
 	let cellsX = 10, cellsY = 10;
 	let noOfMines = 10;
 
-	let gameState = 
+	let gameState =
 	{
 		cellsX: 10,
 		cellsY: 10,
@@ -18,9 +17,8 @@ function main()
 
 	// Generate grid
 	let gameBoard = document.querySelector(".game-board");
-	
-	for(let i = 0; i < gameState.cells.length; i++)
-	{
+
+	for (let i = 0; i < gameState.cells.length; i++) {
 		let child = document.createElement("div");
 		child.setAttribute("class", "cell");
 		child.dataset.cellid = i;
@@ -28,29 +26,23 @@ function main()
 		gameState.cells[i] = child;
 	}
 
-	for(let i = 0; i < noOfMines; i++)
-	{	
-		let loop = true;		
-		while (loop)
-		{
+	for (let i = 0; i < noOfMines; i++) {
+		let loop = true;
+		while (loop) {
 			let rand = Math.floor(Math.random() * gameState.cells.length);
-			if (!gameState.mineLocations.has(rand))
-			{
+			if (!gameState.mineLocations.has(rand)) {
 				gameState.mineLocations.add(rand);
 				loop = false;
 			}
 		}
 	}
 
-	// Calculate adjacency hints
-	for(let i = 0; i < gameState.cells.length; i++)
-	{
+	// Calculate and set adjacency hints
+	for (let i = 0; i < gameState.cells.length; i++) {
 		let count = 0;
-		for (let a of calculateAdjacents(i, gameState)) 
-		{
-			for (let b of gameState.mineLocations.values())
-			{
-				if (a == b) {count++;}
+		for (let a of calculateAdjacents(i, gameState)) {
+			for (let b of gameState.mineLocations.values()) {
+				if (a == b) { count++; }
 			}
 		}
 		gameState.cells[i].dataset.adjMines = count;
@@ -65,18 +57,17 @@ function main()
 
 function cellRightClicked(event) {
 
-	switch (event.target.className)
-	{
-		case "cell": 
-		{
-			event.target.className = "cell-flagged";
-			break;
-		}
+	switch (event.target.className) {
+		case "cell":
+			{
+				event.target.className = "cell-flagged";
+				break;
+			}
 		case "cell-flagged":
-		{
-			event.target.className = "cell";
-			break;
-		}
+			{
+				event.target.className = "cell";
+				break;
+			}
 		default: break;
 	}
 	event.preventDefault();
@@ -96,83 +87,77 @@ function calculateAdjacents(cell, gameState) {
 	adj.set("SE", cell + gameState.cellsX + 1);
 
 	//Top Edge
-	if (cell < gameState.cellsX) { 
+	if (cell < gameState.cellsX) {
 		adj.delete("NW");
 		adj.delete("NE");
 		adj.delete("N");
 	}
 	// Left Edge
-	if (cell % gameState.cellsX == 0) { 
+	if (cell % gameState.cellsX == 0) {
 		adj.delete("W");
 		adj.delete("SW");
 		adj.delete("NW");
 	}
 	// Right Edge
-	if (cell % gameState.cellsX == gameState.cellsX) {
+	if ((cell) % gameState.cellsX == gameState.cellsX - 1) {
 		adj.delete("E");
 		adj.delete("NE");
 		adj.delete("SE");
 	}
 	// Bottom Edge
-	if (cell > (gameState.cells.length - gameState.cellsX)) {
+	if (cell >= (gameState.cells.length - gameState.cellsX)) {
 		adj.delete("S");
 		adj.delete("SW");
-		adj.delete("SE");		
+		adj.delete("SE");
 	}
 	return adj.values();
 }
 
+function processCell(cell, gameState) {
 
-function processCell(cell, gameObject) {
-
-	switch (gameObject.cells[cell].className)
-	{
-		case "cell": 
-		{
-			if (gameObject.mineLocations.has(parseInt(gameObject.cells[cell].dataset.cellid)))
+	switch (gameState.cells[cell].className) {
+		case "cell":
 			{
-				//DO FAILURE STUFF
-				gameObject.cells[cell].className = "cell-clicked-mine";
-				for(let mines of gameObject.mineLocations)
-				{
-					if (!(event.target.dataset.cellid == mines))
-						{
-							gameObject.cells[mines].className = "cell-mine"
+				if (gameState.mineLocations.has(parseInt(gameState.cells[cell].dataset.cellid))) {
+					//DO FAILURE STUFF
+					gameState.cells[cell].className = "cell-clicked-mine";
+					for (let mines of gameState.mineLocations) {
+						if (!(event.target.dataset.cellid == mines)) {
+							gameState.cells[mines].className = "cell-mine"
 						}
-				}
-			}
-			else 
-			{
-				//UNCOVER SQUARE
-				gameObject.cells[cell].className = "cell-clicked";
-				if (gameObject.cells[cell].dataset.adjMines != 0)
-				{
-					gameObject.cells[cell].textContent = event.target.dataset.adjMines;
-				}
-				else
-				{
-					let cellsToCheck = [gameObject.cells[cell].dataset.cellid];
-
-					while (cellsToCheck.length > 0) 
-					{
-						let currentCell = cellsToCheck.pop();
-						for (let adj in calculateAdjacents(currentCell, gameObject)) {
-
-						}			
 					}
 				}
+				else {
+					let cellsToCheck = [cell];
+					while (cellsToCheck.length > 0) {
+						cell = cellsToCheck.pop();
+						//UNCOVER SQUARE
+						gameState.cells[cell].className = "cell-clicked";
+						if (gameState.cells[cell].dataset.adjMines != 0) {
+							gameState.cells[cell].textContent = gameState.cells[cell].dataset.adjMines;
+						}
+						else {
+							for (let adj of calculateAdjacents(cell, gameState)) {
+								if (gameState.cells[adj].className == "cell" && !cellsToCheck.includes(adj)) {
+									cellsToCheck.push(adj);
+								}
+							}
+						}
+
+					}
+				}
+				break;
 			}
-			break;
-		}
 		case "cell-click":
 		case "cell-flagged":
 		default: break;
 	}
+
 }
 
 
 function cellClicked(event) {
-	processCell(event.target.dataset.cellid, this);
+	processCell(parseInt(event.target.dataset.cellid), this);
 }
 
 document.addEventListener("DOMContentLoaded", evt => main());
